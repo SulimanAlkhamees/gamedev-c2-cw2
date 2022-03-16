89911bdfa76109ac52b7b132ff5ca4a965a3937a
SulimanAlkhamees <salooom2006@gmail.com>
2022-03-16 15:33:13 +0300

    summary
    mmk

---
+++ c28/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace c28
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string heroName = "Bakugo";
+          double heroHeight = 1.65;
+            int heroAge = 15;
+            string heroSuperPower1 = "bangs";
+            string heroSuperPower2 = "fire";
+            string heroName2 = "shoto";
+            double heroHeight2 = 1.62;
+            int heroAge2 = 15;
+            string yheroSuperPower1 = "ice";
+            string yheroSuperPower2 = "fire";
+
+            int ageDifference = heroAge - heroAge2;
+
+            Console.WriteLine("the difference of age=" + ageDifference);
+
+            double heightCheck = heroHeight - heroHeight2;
+
+            Console.WriteLine("the difference of Height=" + heightCheck);
+
+
+
+
+            }
+
+
+
+
+
+
+        }
+    }
